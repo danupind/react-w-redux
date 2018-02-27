72bbac798c74d1badac16f3aff8c8ecec9b02b8a
Deepti Anupindi <danupindi@shutterfly.com>
2018-02-27 15:15:01 -0700

    videolist is ready

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAln0kHnTtbIsoo_aZ0VRrWou5eAbkduKo';
-// create a new component. This should produce some html.
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+// create a new component. This should produce some html.
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'foodwishes'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
-
-
 // take this component's generated html and put it on the page in the dom.
-
 ReactDOM.render(<App />, document.querySelector('.container'));
